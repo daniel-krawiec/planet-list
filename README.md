f1492b9832cd823556cd5d8fc686140f470d1549
Daniel <danielkrawiec@op.pl>
2024-08-03 20:00:22 +0200

    project setup add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# planet-list
+# Project setup
+
+npm install
+
+## Compiles and hot-reloads for development
+
+npm run dev
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <PlanetList />
+</template>
+<script setup lang="ts">
+import PlanetList from './views/PlanetList.vue'
+</script>
+<style scoped></style>
